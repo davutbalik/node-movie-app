ffda669589f5c9f13aabe6cdbb1c89a2e8c6bda1
davutbalik <davutbalik@gmail.com>
2021-02-21 17:48:27 +0300

    Adde movi.routes.js and added moviesRouter app use /api/moviesto app.js

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var moviesRouter = require('./routes/movie.routes');
 //db connect
 const db = require('./helpers/db')();
+
+
 
 
 var app = express();
@@ -24,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/movies', moviesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
