c5ab7d99b99a718422b16c64fc1e2d74e74c5603
davutbalik <davutbalik@gmail.com>
2021-02-25 20:18:11 +0300

    ok

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-
 //MODEL
 const MovieModel = require("../models/Movie");
+
+var bodyParser = require('body-parser');
+router.use(bodyParser.json());
+
 
 //GET
 router.get("/", (req, res) => {
@@ -16,15 +19,17 @@ router.get("/", (req, res) => {
 });
 
 // GET A MOVIE
-router.get("/:movieId", (req, res) => {
+router.get("/:movieId", (req, res, next) => {
   MovieModel.findById(req.params.movieId)
-    .then((movieId) => {
-      res.json(movieId);
+    .then((movieList) => {
+      res.json(movieList);
     })
-    .catch((errorMsg) => {
-      res.json(errorMsg);
+    .catch((err) => {
+      next({ message: "The movie was not found!", code: 99 });
+      res.json(err);
     });
 });
+
 
 //POST
 router.post("/", function (req, res) {
@@ -44,5 +49,6 @@ router.post("/", function (req, res) {
       res.json(err);
     });
 });
+
 
 module.exports = router;
