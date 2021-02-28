c871f5f90f74d7259e2d1a2568075bcbcec5a49e
davutbalik <davutbalik@gmail.com>
2021-02-28 15:46:52 +0300

    chai.request.get.end.(err,res)res.should.have.status

--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,20 @@
-const chai = require('chai')
-const chaiHttp = require('chai-http')
-const should = chai.should()
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const should = chai.should();
 
-const server = require('../app')
+const server = require("../app");
 
-chai.use(chaiHttp)
+chai.use(chaiHttp);
 
-describe('Node Server',()=>{
-    it('Run the Homepage (GET/)',(done)=>{
+describe("Node Server", () => {
+  it("Run the Homepage (GET/)", (done) => {
+    //localhost:3000/
+    chai
+      .request(server)
+      .get("/")
+      .end((err, res) => {
+        res.should.have.status(200);
         done();
-    })
-})
+      });
+  });
+});
